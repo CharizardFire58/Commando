bbb55d73e17c1af773367591c7b4c8e0ca75b579
CharizardFire58 <jd.maceirasg@gmail.com>
2018-08-23 02:18:18 -0430

    music commit (anti error) --pre relase--

--- comandos/music/join.js
+++ comandos/music/join.js
@@ -23,7 +23,7 @@ module.exports = class MusicPlayCommand extends Command {
         return new Promise((resolve, reject) => {
 			const voiceChannel = msg.member.voiceChannel;
 			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+			voiceChannel.join()
 		});
     }
 };
