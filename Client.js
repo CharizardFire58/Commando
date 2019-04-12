4ac1185c2446c3824729a889fddc813accf6a1ef
CharizardFire58 <30486280+CharizardFire58@users.noreply.github.com>
2019-04-12 16:02:29 +0000

    Update Client.js

--- Client.js
+++ Client.js
@@ -29,8 +29,37 @@ client.on('ready', () => {
   console.log("ola")
   client.user.setGame("con los nehgrozh.")
 })
+	
+const events = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+};
+
+	.on('raw', async event => {
+		if (!events.hasOwnProperty(event.t)) return;
+
+		const { d: data } = event;
+		const user = client.users.get(data.user_id);
+		const channel = client.channels.get(data.channel_id) || await user.createDM();
+
+		if (channel.messages.has(data.message_id)) return;
+
+		const message = await channel.fetchMessage(data.message_id);
+		const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+		let reaction = message.reactions.get(emojiKey);
+
+		if (!reaction) {
+			const emoji = new Discord.Emoji(client.guilds.get(data.guild_id), data.emoji);
+			reaction = new Discord.MessageReaction(message, emoji, 1, data.user_id === client.user.id);
+		}
+
+		client.emit(events[event.t], reaction, user);
+	})
 	.on('messageReactionAdd', (reaction, user) => {
-		reaction.channel.send(`${user.username} reacted with "${reaction.emoji.name}".`);
+		console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
+	})
+	.on('messageReactionRemove', (reaction, user) => {
+		console.log(`${user.username} removed their "${reaction.emoji.name}" reaction.`);
 	})
 	.on('message', async message => {
 		if (message.channel.type === 'dm') return;
